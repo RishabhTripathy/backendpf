6de89f9a5e4c81aa622505044f24f8731d77b2fe
Rishabh Tripathy <rishabh@Rishabhs-MacBook-Pro.local>
2024-11-27 15:19:06 +0530

    port

--- config/server.js
+++ config/server.js
@@ -1,10 +1,11 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),  // Ensure it binds to all IPs
-  port: env.int('PORT', 10000),
+  host: env("HOST", "0.0.0.0"), // Bind to all available hosts
+  port: env.int("PORT", 1337), // Use the PORT environment variable
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS") // For secure cookie signing (ensure APP_KEYS is set)
   },
+
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false)
+  }
 });
